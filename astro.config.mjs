cfc916fef30e6e6dd133b4af9dee87ee4a6b79d9
fedor <feedoor.feedoot@gmail.com>
2026-01-29 13:33:43 +0300

    aaaaaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-    output: 'server', // нужен для server endpoints
+    output: 'server',
     adapter: vercel(),
 });
